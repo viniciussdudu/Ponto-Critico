646515b9fbc3d6927ad5f8f2bad4209828055ec6
SamaraCdS-UFT <alien2000futurista@gmail.com>
2026-04-22 09:47:43 -0300

    chore: forçando atualização de comparação

--- App/Views/perfil.php
+++ App/Views/perfil.php
@@ -4,7 +4,7 @@
 <title>Meu Perfil - Ponto Crítico</title>
 </head>
 <body>
-<h1>Perfil do Usuário</h1>
+<h1>Perfil do  Usuário</h1>
 <p><strong>Nome:</strong> <?php echo $usuario['nome']; ?></p>
 <p><strong>Email:</strong> <?php echo $usuario['email']; ?></p>
 <a href="index.php?url=editar_perfil">Editar Perfil</a>
